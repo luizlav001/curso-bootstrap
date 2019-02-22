fac6537b9e9e179feb4d844e772753ef273373de
luizlav001 <45859696+luizlav001@users.noreply.github.com>
2019-02-21 21:13:42 -0300

    Header fixo com sombra dinâmica

--- js/main.js
+++ js/main.js
@@ -7,9 +7,20 @@
 // }, 5000)
 
 $('#vendas_collapse').on('show.bs.collapse', function () {
-  $('a[href="#vendas_collapse"]').text("Ver menos")
+  $('a[href="#vendas_collapse"]').text("Ver menos");
 })
 
 $('#vendas_collapse').on('hidden.bs.collapse', function () {
-  $('a[href="#vendas_collapse"]').text("Ver mais")
+  $('a[href="#vendas_collapse"]').text("Ver mais");
+})
+
+$(window).on("scroll", function(){
+	var distanciaDoTopo = $(window).scrollTop();
+
+	if (distanciaDoTopo > 400 ) {
+		distanciaDoTopo = 400
+	}
+
+	$("#header_shadow").css("top", (distanciaDoTopo / 15) + "px")
+
 })
